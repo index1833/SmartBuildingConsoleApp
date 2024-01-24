7a3ddf546a657e8d9c2281474acb2f570ac9891e
Shrimp <tpwns0429@gmail.com>
2024-01-24 18:09:27 +0900

    초기 커밋.

---
+++ SmartBuildingConsoleApp/DigitalTwins/DigitalTwinsManager.cs
@@ -0,0 +1,83 @@
+﻿using Azure;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using SmartBuildingConsoleApp.DigitalTwins;
+using System;
+using System.IO;
+namespace SmartBuildingConsoleApp.DigitalTwins
+{
+    public class DigitalTwinsManager
+    {
+        private static readonly string adtInstanceUrl = "https://DTBDigitalTwins19.api.krc.digitaltwins.azure.net";
+        private DigitalTwinsClient client;
+        public DigitalTwinsManager()
+        {
+            Connect();
+        }
+        public void Connect()
+        {
+            var cred = new DefaultAzureCredential();
+            client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
+        }
+
+        public bool CreateModel(string path)
+        {
+            return CreateModels(new string[] { path });
+        }
+        public bool CreateModels(string[] path)
+        {
+            List<string> dtdls = new List<string>();
+            foreach (string p in path)
+            {
+                using var modelStreamReader = new StreamReader(p);
+                string dtdl = modelStreamReader.ReadToEnd();
+                dtdls.Add(dtdl);
+            }
+            try
+            {
+                DigitalTwinsModelData[] models = client.CreateModels(dtdls.ToArray());
+            }
+            catch (RequestFailedException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void DeleteModel(string modelId)
+        {
+            client.DeleteModel(modelId);
+        }
+
+        public bool CreateDigitalTwin(string twinId, string modelId)
+        {
+            BasicDigitalTwin digitalTwin = new BasicDigitalTwin();
+            digitalTwin.Metadata = new DigitalTwinMetadata();
+            digitalTwin.Metadata.ModelId = modelId;
+            digitalTwin.Id = twinId;
+            try
+            {
+                client.CreateOrReplaceDigitalTwin<BasicDigitalTwin>(twinId, digitalTwin);
+            }
+            catch (RequestFailedException)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool UpdateDigitalTwin(string twinId, string property, object value)
+        {
+            try
+            {
+                BasicDigitalTwin digitalTwin = client.GetDigitalTwin<BasicDigitalTwin>(twinId);
+                digitalTwin.Contents[property] = value;
+                client.CreateOrReplaceDigitalTwin<BasicDigitalTwin>(twinId, digitalTwin);
+            }
+            catch (RequestFailedException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SmartBuildingConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Azure.DigitalTwins.Core;
+using SmartBuildingConsoleApp.DigitalTwins;
+using System;
+
+namespace SmartBuildingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalTwinsManager dtHelper = new DigitalTwinsManager();
+            // TODO
+          
+                        string[] paths = new string[] {
+                            "Models/chapter4/campus.json",
+                            "Models/chapter4/building.json",
+                            "Models/chapter4/floor.json",
+                            "Models/chapter4/workarea.json",
+                            "Models/chapter4/sensor.json"                
+                        };
+                        dtHelper.CreateModels(paths);
+
+            // Meetingroom 추가
+            // string path = "Models/Chapter4/meetingroom.json";
+            //dtHelper.CreateModel(path);
+
+
+            // Delete Modle
+
+            /*dtHelper.DeleteModel("dtmi:com:smartbuilding:Meetingroom;1");*/
+
+            // Get Model
+            /*  DigitalTwinsModelData model = dtHelper.GetModel("dtmi:com:smartbuilding:Room;1");
+              Console.WriteLine(model.Id);
+              Console.WriteLine(model.LanguageDisplayNames["en"]);
+
+
+              var models = dtHelper.GetModels();
+              foreach (DigitalTwinsModelData model in models)
+              {
+                  Console.WriteLine(model.Id);
+                  Console.WriteLine(model.LanguageDisplayNames["en"]);
+              }
+            */
+
+/*            dtHelper.CreateDigitalTwin("Campus", "dtmi:com:smartbuilding:Campus:1");
+            dtHelper.CreateDigitalTwin("MainBuilding", "dtmi:com:smartbuilding:Building:1");
+            dtHelper.CreateDigitalTwin("GroundFloor", "dtmi:com:smartbuilding:Floor:1");
+            dtHelper.CreateDigitalTwin("MeetingRoom1.01", "dtmi:com:smartbuilding:Meetingroom:1");*/
+            //dtHelper.UpdateDigitalTwin("MeetingRoom1.01", "occupied", true);
+        }
+    }
+}
